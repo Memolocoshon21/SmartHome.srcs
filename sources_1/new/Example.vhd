dc3c038c6e77e7dd8eba3094b9ff78535278c524
Sergio Guillermo Trujillo Hernandez <ajedrezsgth@gmail.com>
2026-04-30 21:23:16 -0600

    First Commitment with an example.

---
+++ sources_1/new/Example.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/30/2026 09:17:37 PM
+-- Design Name: 
+-- Module Name: Example - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Example is
+    Port ( example_in : in STD_LOGIC;
+           example_out : in STD_LOGIC);
+end Example;
+
+architecture Behavioral of Example is
+
+begin
+
+
+end Behavioral;
